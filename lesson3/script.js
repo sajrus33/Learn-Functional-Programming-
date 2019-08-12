7b63d4f2d051c79056cc5e7c3269589161951f4f
sajrus33 <brianwala22@gmail.com>
2019-08-12 10:07:55 +0200

    lesson3

--- lesson3/script.js
+++ lesson3/script.js
@@ -4,4 +4,9 @@ const orders = [
     { amount: 400 },
     { amount: 100 },
     { amount: 325 },
-]; const totalAmount =
+];
+// sum is the second parameter of reduce() and passed to it as a first argument
+const totalAmount = orders.reduce((sum, order) => {
+    console.log("hello", sum, order);
+    return sum + order.amount;
+}, 0);
